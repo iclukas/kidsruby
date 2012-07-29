c073047da81227ddde4e6f4f680d9845b71a5ca2
iclukas <acmepwnz@gmail.com>
2012-07-29 22:56:55 +0200

    added German UI file de.js

---
+++ public/js/i18n/de.js
@@ -0,0 +1,18 @@
+{
+  "buttons": {
+    "run": "Ausführen",
+    "clear": "Löschen",
+    "open": "Öffnen",
+    "save": "Speichern",
+    "stop": "Stopp"
+  },
+  "tabs": {
+    "help-link": "Hilfe",
+    "output-link": "Ausgabe",
+    "turtle-link": "Schildkröte"
+  },
+  "editor": {
+    "your-code": "Schreibe dein Programm unter diese Zeile",
+    "confirm-clear": "Möchtest du wirklich das gesamte Programm aus dem Bearbeitungsfenster löschen?"
+  }
+}
